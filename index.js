168f979aed7ad3739df65669740fd1d6af8af93e
Kirstin Kleyn <57179572+kmkleyn@users.noreply.github.com>
2020-05-11 13:04:05 +0200

    Added comments to index.js
    Added comments to index.js to better explain what is happening.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+// Create random numbers between 1 and 6
 var randomNumber1 = Math.ceil(Math.random() * 6);
 var randomNumber2 = Math.ceil(Math.random() * 6);
+
+// Select the src attribute of each image and
+// update it with the image that matches the
+// random number.
 $(".img1").attr("src", "images/dice" + randomNumber1 + ".png");
 $(".img2").attr("src", "images/dice" + randomNumber2 + ".png");
 
+// Change the h1 tag text depending on which number
+// was bigger. &#128681; is the flag emoticon.
 if (randomNumber1 > randomNumber2) {
 	$("h1").html("&#128681; Player 1 Wins!");
 } else if (randomNumber2 > randomNumber1) {
